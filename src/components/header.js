b4665046d5c8ac8abecd06c2737ffaa17d8e506b
Keanu Freitas <freitdev@gmail.com>
2020-01-23 18:11:59 -1000

    I removed all default Gatsby stuff and made the site more company focused. Just a couple images icon change and some text to tell customer that we are currently buidling the site.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: '#393E46',
       marginBottom: `1.45rem`,
     }}
   >
